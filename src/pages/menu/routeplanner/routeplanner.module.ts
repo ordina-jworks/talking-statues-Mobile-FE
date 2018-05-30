371e1d8d7104300168a67a844e5f4b906d56af31
Jef Willems <jwillems04@gmail.com>
2018-05-30 07:37:44 +0200

    installed socket.io to use for real chat traffic

--- src/pages/menu/routeplanner/routeplanner.module.ts
+++ src/pages/menu/routeplanner/routeplanner.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { RouteplannerPage } from './routeplanner';
-import { ChoisePage } from './choise/choise';
-import { InfluencerPage } from './influencer/influencer';
-import { ChoisePageModule } from './choise/choise.module';
-import { InfluencerPageModule } from './influencer/influencer.module';
 
 @NgModule({
   declarations: [
