73453f8e5882c9095b9ed59b3999047133d552d5
PeDK <peter.dekinder@ordina.be>
2018-06-20 13:49:57 +0200

    TS-12 Added css configs

--- src/pages/menu/info/info.ts
+++ src/pages/menu/info/info.ts
@@ -20,7 +20,7 @@ export class InfoPage {
   image;
   monuments: Monument[] =[
     {
-      id:'5b29f15d95fe13000164ba0e',
+      id:'5b2a0d3fa7b11b0001aa16e4',
       information:[{
         name: 'de NEUS',
         language:Language.NL,
@@ -29,18 +29,18 @@ export class InfoPage {
         "Hij dacht er niet aan de handen uit de mouwen te steken als hij nog enkele franken op zak had.\n" +
         "In de Spiegelpoort van de Nationalestraat bevond zich de stapelplaats van houthandelaar Blanckaert.  " +
         "De café's in de buurt hadden aan hem een goede klant.  " +
-        "Frans leed namelijk aan chronische dorst die hij probeerde te lessen met romers \\" +
-        "Pinard\",een soort rode wijn.  Vaste klant was hij in het \\\"Pinardkot\\\" op de hoek van de Lange Vlierstraat en het pleintje aan de Nationalestraat.\\n" +
-        "Als Frans in het \\\"Pinardkot\\\" honger kreeg, kocht hij een pak frieten met pickels en een worst.  " +
-        "Of\\nhij haalde in de Steenbergstraat een \\\"gardevil\\\", een gerookte haring die hij met vel en graten\\nnaar binnen slokte.  " +
-        "Natuurlijk werden frieten of \\\"gardevils\\\" flink doorgespoeld met enorme hoeveelheden \\\"Pinard\\\".\\n" +
-        "Ook hielpen de lossers en de laders van Blanckaert elkaar aan eten.  Zo had Jef, de chauffeur,\\nFrans de overschot van aardappelen met stoofkarbonaden aangeboden.  " +
-        "Die had hongerige Frans gretig aangenomen.  Hij at alles likkebaardend op zonder te merken dat Jef de aardappelen en karbonaden had opgemaakt met gestampte rode baksteen en roet van de kachel.\\n" +
+        "Frans leed namelijk aan chronische dorst die hij probeerde te lessen met romers \"" +
+        "Pinard\",een soort rode wijn.  Vaste klant was hij in het \"Pinardkot\" op de hoek van de Lange Vlierstraat en het pleintje aan de Nationalestraat.\n" +
+        "Als Frans in het \"Pinardkot\" honger kreeg, kocht hij een pak frieten met pickels en een worst.  " +
+        "Of\nhij haalde in de Steenbergstraat een \"gardevil\", een gerookte haring die hij met vel en graten\nnaar binnen slokte.  " +
+        "Natuurlijk werden frieten of \"gardevils\" flink doorgespoeld met enorme hoeveelheden \"Pinard\".\n" +
+        "Ook hielpen de lossers en de laders van Blanckaert elkaar aan eten.  Zo had Jef, de chauffeur,\nFrans de overschot van aardappelen met stoofkarbonaden aangeboden.  " +
+        "Die had hongerige Frans gretig aangenomen.  Hij at alles likkebaardend op zonder te merken dat Jef de aardappelen en karbonaden had opgemaakt met gestampte rode baksteen en roet van de kachel.\n" +
         "Eens wilde Frans de Neus een biefstuk braden.  Terwijl deze op het gasvuur in de pan lag, was Frans in slaap gevallen.  Plots werd hij wakker door een brandgeur.  " +
         "Hij probeerde het vuur te blussen en zocht naar een fles water, maar hij goot er per vergissing een fles petroleum over.  " +
         "Toen het brandje heviger begon te laaien wierp hij er zijn jas over om het te doven.  " +
-        "Toen men\\nFrans de Neus maanden later tegenkwam rook zijn jas nog naar verbrand goed.\\n" +
-        "Hij blijft in de herinnering als één van de stevigste Pinard-drinkers uit het Sint-Andrieskwartier.\\n",
+        "Toen men\nFrans de Neus maanden later tegenkwam rook zijn jas nog naar verbrand goed.\n\n" +
+        "Hij blijft in de herinnering als één van de stevigste Pinard-drinkers uit het Sint-Andrieskwartier.\n",
         question:[],
       }],
       area:"Sint-Andries",
