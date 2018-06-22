ead7aa98c172626e2b10a22c252dd43c48d85da6
Jef Willems <jwillems04@gmail.com>
2018-06-22 14:09:38 +0200

    removed unused code in login ts file

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { Events, IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MenuPage } from '../menu/menu';
-import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
+import { Facebook } from '@ionic-native/facebook';
 import { LoginService } from '../../services/login.service';
-
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -18,19 +11,15 @@ import { LoginService } from '../../services/login.service';
 })
 export class LoginPage {
   userData: any;
-  isLoggedIn: boolean = false;
 
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams,
-    private facebook: Facebook,
     private _loginService: LoginService,
   ) {
     this._loginService.checkLoggedUserID().subscribe(
       res => {
         this.userData = res;
         console.log('Logged in UserID: ',this.userData);
-
         if (this.userData) {
           this.navCtrl.push(MenuPage);
         }
