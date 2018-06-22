6962bfd234990b2fa69ac31e4818766bc759a574
Jef Willems <jwillems04@gmail.com>
2018-06-22 13:38:23 +0200

    push clicked marker data to info page, and show in html info page

--- src/pages/menu/info/info.ts
+++ src/pages/menu/info/info.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Facebook } from '@ionic-native/facebook';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { Language, Monument } from '../../../app/models/monument';
 
-/**
- * Generated class for the InfoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-info',
@@ -17,67 +10,18 @@ import { Language, Monument } from '../../../app/models/monument';
 export class InfoPage {
   monumentData: Monument[];
 
-  image;
-  monuments: Monument[] =[
-    {
-      id:'5b29f15d95fe13000164ba0e',
-      information:[{
-        name: 'de NEUS',
-        language:Language.NL,
-        description: "Frans was een grote kerel die bang was van werken.  " +
-        "Hij was niet gehuwd en sliep steeds gekleed op wat zakkengoed.  " +
-        "Hij dacht er niet aan de handen uit de mouwen te steken als hij nog enkele franken op zak had.\n" +
-        "In de Spiegelpoort van de Nationalestraat bevond zich de stapelplaats van houthandelaar Blanckaert.  " +
-        "De café's in de buurt hadden aan hem een goede klant.  " +
-        "Frans leed namelijk aan chronische dorst die hij probeerde te lessen met romers \\" +
-        "Pinard\",een soort rode wijn.  Vaste klant was hij in het \\\"Pinardkot\\\" op de hoek van de Lange Vlierstraat en het pleintje aan de Nationalestraat.\\n" +
-        "Als Frans in het \\\"Pinardkot\\\" honger kreeg, kocht hij een pak frieten met pickels en een worst.  " +
-        "Of\\nhij haalde in de Steenbergstraat een \\\"gardevil\\\", een gerookte haring die hij met vel en graten\\nnaar binnen slokte.  " +
-        "Natuurlijk werden frieten of \\\"gardevils\\\" flink doorgespoeld met enorme hoeveelheden \\\"Pinard\\\".\\n" +
-        "Ook hielpen de lossers en de laders van Blanckaert elkaar aan eten.  Zo had Jef, de chauffeur,\\nFrans de overschot van aardappelen met stoofkarbonaden aangeboden.  " +
-        "Die had hongerige Frans gretig aangenomen.  Hij at alles likkebaardend op zonder te merken dat Jef de aardappelen en karbonaden had opgemaakt met gestampte rode baksteen en roet van de kachel.\\n" +
-        "Eens wilde Frans de Neus een biefstuk braden.  Terwijl deze op het gasvuur in de pan lag, was Frans in slaap gevallen.  Plots werd hij wakker door een brandgeur.  " +
-        "Hij probeerde het vuur te blussen en zocht naar een fles water, maar hij goot er per vergissing een fles petroleum over.  " +
-        "Toen het brandje heviger begon te laaien wierp hij er zijn jas over om het te doven.  " +
-        "Toen men\\nFrans de Neus maanden later tegenkwam rook zijn jas nog naar verbrand goed.\\n" +
-        "Hij blijft in de herinnering als één van de stevigste Pinard-drinkers uit het Sint-Andrieskwartier.\\n",
-        question:[],
-      }],
-      area:"Sint-Andries",
-      picture: 'statue1.jpg',
-      // imageRef: 'https://images.standbeelden.be/600x0/1363/Antoon%20Van%20Dyck.jpg',
-      latitude: 51.2149218763,
-      longitude: 4.398393631
-    }
-  ];
-
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private fb: Facebook,
+    public viewCtrl: ViewController,
   ) {
-    // this.monumentData = this.monuments.map(m => m.information);
-    this.monumentData = this.monuments;
+    this.monumentData = navParams.get('infoData');
     console.log(this.monumentData);
-    this.image = `http://localhost:9000/monuments/${this.monumentData[0].id}/image`
-    // fb.getLoginStatus()
-    //   .then(data => {
-    //     this.getUserDetail(data.authResponse.userID);
-    //   })
   }
 
-  ionViewDidLoad() {
-    // console.log('ionViewDidLoad InfoPage');
+  public onDismiss(): void {
+    this.viewCtrl.dismiss();
   }
 
-  // getUserDetail(userid) {
-  //   this.fb.api("/"+userid+"/?fields=id,email,name,picture,gender",["public_profile"])
-  //     .then(res => {
-  //       this.userData = res;
-  //     })
-  //     .catch(e => {
-  //       console.log(e);
-  //     });
-  // }
 
 }
--- src/pages/menu/my-route/navigationmap/navigationmap.ts
+++ src/pages/menu/my-route/navigationmap/navigationmap.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { Geolocation} from '@ionic-native/geolocation';
-import { Monument, Route } from '../../../../app/models/monument';
+import { Route } from '../../../../app/models/monument';
 import { InfoPage } from '../../info/info';
 import { Nav } from 'ionic-angular';
 
@@ -26,6 +26,7 @@ export class NavigationmapPage {
   destination: any;
   infoWindows: any;
 
+
   directionsService = new google.maps.DirectionsService();
   directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true});
 
@@ -36,7 +37,6 @@ export class NavigationmapPage {
     public params: NavParams,
     public viewCtrl: ViewController,
   ) {
-    let Key = "AIzaSyB8XRZJKwlGjlWu15KRI7j-6VFT_LL9TDE";
     this.receivedData = params.get('data');
     this.title = this.receivedData.routeTitle;
     this.infoWindows = [];
@@ -46,7 +46,8 @@ export class NavigationmapPage {
     this.displayGoogleMap();
   }
 
-  displayGoogleMap() {
+
+  public displayGoogleMap(): void {
     // everything starts with the current position coords
     let watch = this.geolocation.getCurrentPosition();
     watch.then((position) => {
@@ -59,13 +60,6 @@ export class NavigationmapPage {
       this.map = new google.maps.Map(document.getElementById('map'), {
         zoom: 8,
         center: this.pos
-      });
-
-      // needed to implement in directions api logic
-      this.receivedData.monuments.map(monument => {
-        this.monumentLatitude = monument.latitude,
-          this.monumentLongitude = monument.longitude,
-          this.markers.push(monument.latitude +','+ monument.longitude)
       });
 
       // get all liked monuments their coords in a marker
@@ -85,52 +79,59 @@ export class NavigationmapPage {
         maxWidth: 350
       });
 
-      // code for info windows per marker
-      for(let marker of mapMarkers) {
-
-        marker.addListener('click', function () {
-          var content = document.createElement('div'),
-            button;
-          content.innerHTML = 'My monument name is   ' + marker.title;
-          button = content.appendChild(document.createElement('input'));
-          button.type = 'button';
-          button.value = 'Click here to see my info.';
-
-          // code attempt to get data from JS code into Angular: from the on infowindow click event
-          google.maps.event.addDomListener(button, 'click', () => {
-            console.log(marker.data);
-            // var navParent = this.navCtrl.parent.parent as NavController;
-            // console.log(navParent);
-            // navParent.push('InfoPage', {
-            //   infoData: console.log(marker.data)
-            // });
-          });
-
-          infowindow.setContent(content);
-          infowindow.open(this.map, marker);
-        });
-      }
-
-      // seperate same logic as monument markers, but for currentlocation marker.
-      var contentString1 = 'Your Current Location.';
-      var infowindow1 = new google.maps.InfoWindow({
-        content: contentString1,
-      });
-
-      var startMarker = new google.maps.Marker({
-        position: this.pos,
-        map: this.map,
-        title: 'your current Position ( Home)'
-      });
-      startMarker.addListener('click', function () {
-        infowindow1.open(this.map, startMarker);
-      });
-
+      this.createMarkers(mapMarkers, infowindow);
+      this.currentLocationMarker();
       this.calculateAndDisplayRoute();
     });
   }
 
-  calculateAndDisplayRoute() {
+  public createMarkers(markers, infowindow): void {
+    for(let marker of markers) {
+
+      marker.addListener('click',() => {
+        var content = document.createElement('div'),
+          button;
+        content.innerHTML = 'My monument name is   ' + marker.title;
+        button = content.appendChild(document.createElement('input'));
+        button.type = 'button';
+        button.value = 'Click here to see my info.';
+
+        // code to get data from JS code into Angular: from the on infowindow click event
+        google.maps.event.addDomListener(button, 'click',() => {
+          this.navCtrl.push(InfoPage, {
+            infoData: marker.data
+          })
+        });
+        infowindow.setContent(content);
+        infowindow.open(this.map, marker);
+       });
+    }
+  }
+
+
+  public currentLocationMarker(): void {
+    var contentString1 = 'Your Current Location.';
+    var infowindow1 = new google.maps.InfoWindow({
+      content: contentString1,
+    });
+    var startMarker = new google.maps.Marker({
+      position: this.pos,
+      map: this.map,
+      title: 'your current Position ( Home)'
+    });
+    startMarker.addListener('click', function() {
+      infowindow1.open(this.map, startMarker);
+    });
+  }
+
+
+  public calculateAndDisplayRoute(): void {
+    // needed to implement in directions api logic
+    this.receivedData.monuments.map(monument => {
+      this.monumentLatitude = monument.latitude,
+        this.monumentLongitude = monument.longitude,
+        this.markers.push(monument.latitude +','+ monument.longitude)
+    });
     let waypts = [];
     for (let i = 0; i < this.markers.length; i++) {
       waypts.push({
@@ -153,14 +154,14 @@ export class NavigationmapPage {
     }, (response, status) => {
       if (status === 'OK') {
         this.directionsDisplay.setDirections(response);
-        console.log(response);
       } else {
         window.alert('Directions request failed due to ' + status);
       }
     });
   }
 
-  onDismiss() {
+
+  public onDismiss(): void {
     this.viewCtrl.dismiss();
   }
 
